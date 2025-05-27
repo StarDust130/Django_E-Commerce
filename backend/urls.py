ad1e50333a1bb3f42dc9b31e80eb59e391ac0fb1
ChandraShekhar <csyadav0513@gmail.com>
2025-05-27 11:56:21 +0530

    Add initial API URL configuration file

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # Include the API URLs
+    # path("api/", include("api.urls")),  # Include the API URLs
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
