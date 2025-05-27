3d9799dd737fd5b0d6caf4e101d13f397d60987e
ChandraShekhar <csyadav0513@gmail.com>
2025-05-27 10:59:05 +0530

    Add custom user model extending Django's AbstractUser

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends the default Django user model.
+    """
+    email = models.EmailField(unique=True)
+    profile_picture_url = models.URLField(
+        max_length=255, blank=True, null=True,
+        help_text="profile picture"
+    )
+
+    def __str__(self):
+        return self.email   # Use email as the string representation of the user
+ 
