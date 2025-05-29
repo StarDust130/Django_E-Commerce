d7a28a08ed7b231f2a39756a60cc144c87480acc
ChandraShekhar <csyadav0513@gmail.com>
2025-05-29 10:53:04 +0530

    Add product detail view and update imports in views.py

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Products
-from .serializers import ProductListSerializer
+from .serializers import ProductListSerializer, ProductDetailSerializer
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
+#! Product List View 😮‍💨(Show Products in Home Page)
 api_view(['GET'])
 def product_list(request):
     products = Products.objects.filter(featured=True)
@@ -14,3 +15,10 @@ def product_list(request):
     return Response(serializer.data)
 
 
+#! Product Detail View 🙃(Show Product Details in Product Page)
+api_view(['GET'])
+def product_detail(request, slug):
+    product = get_object_or_404(Products, slug=slug)
+    serializer = ProductDetailSerializer(product)
+    print("Product Detail Serializer 😆", serializer)  
+    return Response(serializer.data)
