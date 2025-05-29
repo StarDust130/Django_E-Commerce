141ed4c9eb654af41c22fd538605d706ef404681
ChandraShekhar <csyadav0513@gmail.com>
2025-05-29 11:27:38 +0530

    Update URL configurations to include API routes and organize urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,16 @@
 
+from django.urls import path
+from .views import product_list, product_detail, category_list, category_detail
+from .views import CustomUserListView, CustomUserDetailView
+
+
+
+urlpatterns = [
+    path("product_list/", product_list, name="product_list"),
+    path("products/<slug:slug>/", product_detail, name="product-detail"),  
+    path("category_list/", category_list, name="category-list"),
+    path("category_detail/<slug:slug>/",
+         category_detail, name="category-detail"),
+   
+]
+
