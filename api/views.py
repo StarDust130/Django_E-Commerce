befd3d9cffd5328a85694987e66aabea26cb0f59
ChandraShekhar <csyadav0513@gmail.com>
2025-05-29 10:44:33 +0530

    Implement product list view to retrieve featured products

--- api/views.py
+++ api/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Products
+from .serializers import ProductListSerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+api_view(['GET'])
+def product_list(request):
+    products = Products.objects.filter(featured=True)
+    serializer = ProductListSerializer(products, many=True)
+    print("Serializer 😆", serializer)  
+    return Response(serializer.data)
 
 
