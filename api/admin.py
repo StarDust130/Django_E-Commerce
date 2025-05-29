dc018993eccdc65a8b8f417fc08cf57875818e3b
ChandraShekhar <csyadav0513@gmail.com>
2025-05-29 12:46:39 +0530

    Refactor admin.py to improve model registration and enhance code organization

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser , Products , Categories
+
+
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', "featured")
+
+
+admin.site.register( Products , ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+admin.site.register( Categories , CategoryAdmin)
