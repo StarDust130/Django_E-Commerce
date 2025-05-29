c45125b79563fba4634f85cfe1a1ffdab4f7b8d4
ChandraShekhar <csyadav0513@gmail.com>
2025-05-29 11:05:12 +0530

    Fix import for CategoryListSerializer and update category list view to use the correct serializer

--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Products , Categories
-from .serializers import ProductListSerializer, ProductDetailSerializer , CategorySerializer
+from .serializers import ProductListSerializer, ProductDetailSerializer , CategoryListSerializer
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
+# List -> Home Page 
+# Detail -> Product Page
 
 #! Product List View 😮‍💨(Show Products in Home Page)
 api_view(['GET'])
@@ -27,5 +28,12 @@ def product_detail(request, slug):
 api_view(['GET'])
 def category_list(request):
     categories = Categories.objects.all()
-    serializer = CategorySerializer(categories, many=True)
+    serializer = CategoryListSerializer(categories, many=True)
+    return Response(serializer.data)
+
+#! Category Detail View 😚(Show Categories in Home Page)
+api_view(['GET'])
+def category_detail(request, slug):
+    category = get_object_or_404(Categories, slug=slug)
+    serializer = CategoryListSerializer(category)
     return Response(serializer.data)
