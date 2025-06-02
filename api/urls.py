30b50da76f8bbbb42abe69f42a5505ab9b530666
ChandraShekhar <csyadav0513@gmail.com>
2025-06-02 11:10:43 +0530

    Refactor URL patterns for consistency and clarity in comments

--- api/urls.py
+++ api/urls.py
@@ -5,14 +5,14 @@ from .views import add_to_cart, product_list, product_detail, category_list, cat
 
 
 urlpatterns = [
-    path("product_list/", product_list, name="product_list"),
-    path("products/<slug:slug>/", product_detail, name="product-detail"),  
-    path("category_list/", category_list, name="category-list"),
-    path("category_detail/<slug:slug>/", category_detail, name="category-detail"),
+    path("product_list/", product_list, name="product_list"), # Get
+    path("products/<slug:slug>/", product_detail, name="product-detail"),  # Get 
+    path("category_list/", category_list, name="category-list"), # Get
+    path("category_detail/<slug:slug>/", category_detail, name="category-detail"), # Get
 
-    path("add_to_cart/", add_to_cart, name="add-to-cart"),
-    path("update_cartitem_quantity/" , update_cartitem_quantity, name="update-cartitem-quantity"),
-    path("view_cart", view_cart, name="view-cart")
+    path("add_to_cart/", add_to_cart, name="add-to-cart"), # Post
+    path("update_cartitem_quantity/" , update_cartitem_quantity, name="update-cartitem-quantity"), # Put
+    path("view_cart", view_cart, name="view-cart") # Get
    
 ]
 
